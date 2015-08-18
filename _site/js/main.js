6ffc2207ffe6e238a8f98d7f8f9a40dd022d0051
Colin Lebens <colin.lebens@gmail.com>
2015-08-18 12:00:31 -0700

    First publishable version

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready( function() {
 
+    $('.first_school').trigger('click');
+    $('.first_job').trigger('click');
 
 
 });
 
 
 (function() {
-    setup_display_toggle = function(prefix) {
+    util = {};
+
+    util.setup_display_toggle = function(prefix) {
         $("#" + prefix + "-show").on("click", function() {
             $("#" + prefix + "-show").toggleClass("hidden");
             $("#" + prefix + "-hide").toggleClass("hidden");
@@ -19,7 +23,44 @@ $(document).ready( function() {
             $("#" + prefix + "-details").toggleClass("hidden");
         });
 
+        $("#" + prefix + "-name").on("click", function() {
+            if ($("#" + prefix + "-show").hasClass('active'))
+                $("#" + prefix + "-show").trigger("click");
+            else
+                $("#" + prefix + "-hide").trigger("click");
+        });
+
         $("#" + prefix + "-hide").toggleClass("hidden");
         $("#" + prefix + "-details").toggleClass("hidden");
+    };
+
+    util.small_caps = function(str) {
+
+        var full_cap_words = ["PC", "LLO"];
+
+        function capitalize_string(str) {
+            console.log(str);
+            return str.split("").map(function(item) {
+                return '<div class="capital">' +
+                    item +
+                    '</div>';
+            }).join("");
+        }
+
+        var small_cap_ary = str.split(" ").map(function(item) {
+
+            console.log(full_cap_words.indexOf(item.toUpperCase()))
+
+            if (full_cap_words.indexOf(item.toUpperCase()) != -1)
+                return capitalize_string(item);
+            else
+                return '<div class="capital">' +
+                    item[0] +
+                    '</div>' +
+                    item.substring(1);
+
+        });
+
+        return small_cap_ary.join(" ");
     }
 })();
